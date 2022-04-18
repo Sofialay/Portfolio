d857c96ac34264f628a49721c628dcd34866b21d
sofialay <sofi-lay@hotmail.com>
2022-04-18 15:49:27 -0300

    little change

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,7 +8,7 @@ export default function AboutMe() {
             <h2 className='fs-600'>Who I am</h2>
             <p className='fs-400'> I identify as a <i>curious person </i> who loves to learn and try new things. That quality led me to learn to create web pages and find my Front End profession.</p>
             <p className='fs-400'> 
-                Also I'm a podcast fan and sometimes in my free time I do some <a href='https://www.instagram.com/hoppusbrodery/' target='_blank' rel='noreferrer'>brodery</a> / <a href='https://sofialay-raw.tumblr.com/' target='_blank' rel='noreferrer'>photography</a> / drawing.
+                Also I'm a podcast fan and sometimes in my free time I do some embroidery /photography / drawing.
             </p>
         </section>
     )
